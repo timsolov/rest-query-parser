c1dcc09ecfd44741f84a32ae02d44b94600f0121
timsolov <timsolodev@gmail.com>
2020-03-12 20:15:32 +0300

    create self errors type

--- errors.go
+++ errors.go
@@ -2,14 +2,20 @@ package rqp
 
 import "errors"
 
+type Error error
+
+func NewError(msg string) Error {
+	return errors.New(msg)
+}
+
 var (
-	ErrRequired           error = errors.New("required")
-	ErrBadFormat          error = errors.New("bad format")
-	ErrUnknownMethod      error = errors.New("unknown method")
-	ErrNotInScope         error = errors.New("not in scope")
-	ErrSimilarNames       error = errors.New("similar names of keys are not allowed")
-	ErrMethodNotAllowed   error = errors.New("method are not allowed")
-	ErrFilterNotAllowed   error = errors.New("filter are not allowed")
-	ErrFilterNotFound     error = errors.New("filter not found")
-	ErrValidationNotFound error = errors.New("validation not found")
+	ErrRequired           = NewError("required")
+	ErrBadFormat          = NewError("bad format")
+	ErrUnknownMethod      = NewError("unknown method")
+	ErrNotInScope         = NewError("not in scope")
+	ErrSimilarNames       = NewError("similar names of keys are not allowed")
+	ErrMethodNotAllowed   = NewError("method are not allowed")
+	ErrFilterNotAllowed   = NewError("filter are not allowed")
+	ErrFilterNotFound     = NewError("filter not found")
+	ErrValidationNotFound = NewError("validation not found")
 )
